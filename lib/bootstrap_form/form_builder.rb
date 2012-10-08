8d680f92b42d0f54c35cbf3b25b2cb99a77c2a5b
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2012-10-08 11:59:08 +0200

    Adding file field  with thumbnail or link + remove js

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -11,25 +11,84 @@ module BootstrapForm
         @help_tag = :span
         @help_css = 'help-inline'
       end
+
+      @help_after = options.fetch(:help_position, '').to_sym == :after
+
     end
 
     %w{text_field text_area password_field collection_select file_field date_select select}.each do |method_name|
       define_method(method_name) do |name, *args|
+
         options = args.extract_options!.symbolize_keys!
 
         if options[:no_bootstrap]
           super(name, options.except(:label, :help, :no_label, :no_bootstrap))
         else
-          content_tag :div, class: "control-group#{(' error' if object.errors[name].any?)}"  do
+          class_names = ["control-group"]
+          class_names << :error if object.errors[name].any?
+          class_names << :last if options[:last]
+
+
+          content_tag :div, class: class_names.join(" ") do
             (options[:no_label].blank? ? label(name, options[:label], class: 'control-label') : "").html_safe +
             content_tag(:div, class: 'controls') do
               help = object.errors[name].any? ? object.errors[name].join(', ') : options[:help]
               help = content_tag(@help_tag, class: @help_css) { help } if help
-              args << options.except(:label, :help, :no_label, :no_bootstrap)
-              super(name, *args) + help
+              args << options.except(:label, :help, :no_label, :no_bootstrap, :last)
+
+              content = super(name, *args)
+
+              content = content_for method_name, name, content, options
+
+              if @help_after
+                (content + help).html_safe
+              else
+                ((help || "") + content).html_safe
+              end
+
             end
           end
         end
+      end
+    end
+
+    def content_for method_name, field, content, options = {}
+      case method_name.to_sym
+        when :file_field
+          file_field_render(method_name, field, content, options)
+        else
+          content
+      end
+    end
+
+    def image_file_field(field, options = {})
+      file_field(field, options.merge({file_type: :media, paperclip_style: "thumbnail", with_link_helper: true}))
+    end
+
+    def file_field_render(method_name, field, content, options = {})
+      has_uploaded_file = @object.send("#{field}?") && object.id
+
+      if options[:with_link_helper] == true and has_uploaded_file
+        options[:file_type] ||= :file
+
+        case options[:file_type].to_sym
+          when :file, :pdf
+            filelink_html = @template.link_to @object.send("#{field}_file_name"), @object.send("#{field}").url,:target => :blank
+            filename_html = content_tag(:div, content_tag(:i, nil, class: "icon-file") + filelink_html, :style => "margin-top: 10px")
+
+          when :media
+            url = options[:paperclip_style] ? @object.send(field).url(options[:paperclip_style]) : @object.send(field).url()
+
+            filelink_html = options[:paperclip_style] ? @object.send(field).url(options[:paperclip_style]) : @object.send(field).url()
+            filename_html = has_uploaded_file ? @template.image_tag(url, :class => options[:class]) : ""
+        end
+
+        remove_field  = hidden_field((options.delete(:use_remove_attribute) ? "remove_#{field}" : :"_destroy"))
+        remove_link   = content_tag(:div, content_tag(:i, nil, class: "icon-remove") + I18n.t('bridge.form.remove_entry'), :onclick => "remove_attached_file(this)", :class => "remove_link")
+
+        (filename_html + remove_field + remove_link + content).html_safe
+      else
+        content
       end
     end
 
@@ -37,41 +96,60 @@ module BootstrapForm
     %w(i18n_text_field i18n_text_area).each do |method_name|
       define_method(method_name) do |name, *args|
         options = args.extract_options!.symbolize_keys!
-        options.merge!({tag_type: method_name.gsub("i18n_", "").to_sym, no_label: true, no_bootstrap: true})
 
-        translation_field name, options
+        raise "no :languages options defined" if options[:languages].blank?
+
+        options.merge!({tag_type: method_name.gsub("i18n_", "").to_sym})
+
+        # When fields grouped
+        if name.is_a?(Array)
+          options.merge!({fields_grouped: true})
+          translation_fields name, options
+        else
+          options.merge!({no_label: true, no_bootstrap: true})
+
+          translation_fields [name], options
+        end
       end
     end
 
-    def translation_field name, options = {}
-      content_tag :div, class: "control-group#{(' error' if object.errors[name].any?)}"  do
-        label(name, options[:label], class: 'control-label') +
+    def translation_fields names, options
+      # Raise if no major label configured when fields grouped
+      raise "no :major_label configured" if options[:fields_grouped] and options[:major_label].blank?
+
+      content_tag :div, class: "control-group#{(' error' if any_errors_on?(names))}"  do
+        label(options[:fields_grouped] ? options[:major_label] : names.first, options[:label], class: 'control-label') +
         content_tag(:div, class: "tab-container") do
           content_tag(:span, "Translate",class: "translate-title") +
           content_tag(:div) do
-            if object.class.respond_to?(:translates?) && object.class.translates? && object.translated_attribute_names.include?(name.to_sym)
-              default_language = options[:languages].first.locale
-
-              # For the field, construction of the input translated
-              tabs_content = []
-              tabs    = []
-              format  = language_tabs_format(options[:languages])
-              options.delete(:languages).each do |language|
-                new_field = "%s_%s" % [name, language.locale]
-
-                tabs          << construct_tab(object, new_field, default_language, language, format, options)
-                tabs_content  << construct_tab_content(object, name, new_field, default_language == language.locale, options)
-              end
-              content_tag(:ul, tabs.join.html_safe          , class: "nav nav-tabs") +
-              content_tag(:div, tabs_content.join.html_safe , class: "tab-content")
+            if fields_translated?(object, names)
+              construct_tabs(object, names, options)
             else
               raise "no field to translate"
             end
-
           end
         end
       end
+    end
 
+    def construct_tabs object, names, options = {}
+      tabs_content     = []
+      tabs             = []
+      format           = language_tabs_format(options[:languages])
+      default_language = options[:languages].first.locale
+
+      options.delete(:languages).each do |language|
+        new_field = if options[:tag_type] == :file_field
+          names.first
+        else
+          "%s_%s" % [options[:fields_grouped] ? options[:major_label] : names.first, language.locale]
+        end
+
+        tabs         << construct_tab(object, new_field, default_language, language, format, options)
+        tabs_content << construct_tab_content(object, names, options[:fields_grouped] ? new_field : names.first, default_language == language.locale, language.locale, options)
+      end
+      content_tag(:ul, tabs.join.html_safe          , class: "nav nav-tabs") +
+      content_tag(:div, tabs_content.join.html_safe , class: "tab-content")
     end
 
     def datetime_picker name, *args
@@ -150,23 +228,67 @@ module BootstrapForm
 
     def construct_tab(object, field, default_language, language, format = :name, options = {})
       content_tag(:li, class: ('active' if language.locale == default_language)) do
-        content_tag(:a, "data-toggle" => :tab, href: options[:sub_field].present? ? "#%s_%s_%s" % [options[:sub_field], field, object.id] : "#%s_%s" % [field, object.id]) do
+        # If file field, exception for generate id
+        href = if options[:tag_type] == :file_field
+          "%s_%s" % [options[:object], object.send(options[:object]).select{|attachment| attachment.locale == language.locale}.first.locale]
+        else
+          options[:sub_field].present? ? "#%s_%s_%s" % [options[:sub_field], field, object.id] : "#%s_%s" % [field, object.id]
+        end
+
+        content_tag(:a,
+          :"data-toggle" => :tab,
+          :href          =>  href) do
           language[format].humanize
         end
       end
     end
 
-    def construct_tab_content(object, method, field, is_active = false, options = {})
-      content_tag(:div,
-      :id    => options[:sub_field].present? ? "%s_%s_%s" % [options[:sub_field], field, object.id] : "%s_%s" % [field, object.id],
-      :class => "tab-pane #{'active' if is_active}") do
-        case options[:tag_type]
-          when :text_field
-            text_field(field, options.except(:tag_type))
-          when :text_area
-            text_area(field, options.except(:tag_type))
+    def construct_tab_content(object, methods, field, active = false, locale = :fr, options = {})
+      html = []
+
+      tab_pane_for(object, field, locale, active, options) do
+        methods.each_with_index do |method, index|
+
+          options[:label] = object.class.human_attribute_name(method)
+
+          if options[:help] == true
+            help_label = I18n.t("bridge.%s.%s.help.example" % [object.class.to_s.underscore, method.to_s])
+          end
+
+          method          = "%s_%s" % [method, locale] unless options[:tag_type] == :file_field
+
+          html << case options[:tag_type]
+            when :text_field
+              text_field(method, options.except(:tag_type).merge({:help => help_label}))
+            when :text_area
+              text_area(method, options.except(:tag_type).merge({:help => help_label}))
+            when :file_field
+              fields_for options[:object] do |attachment_fields|
+                if attachment_fields.object.locale == locale
+                  attachment_fields.hidden_field(:event_id) +
+                  attachment_fields.hidden_field(:locale) +
+                  attachment_fields.file_field(method, options.except(:tag_type).merge({:help => help_label}))
+                end
+              end
+          end
         end
 
+        html.join().html_safe
+      end
+    end
+
+    def tab_pane_for(object, field, locale, active = false, options, &block)
+      id = if options[:tag_type] == :file_field
+        "%s_%s" % [options[:object], locale]
+      else
+        options[:sub_field].present? ? "%s_%s_%s" % [options[:sub_field], field, object.id] : "%s_%s" % [field, object.id]
+      end
+
+
+      content_tag(:div,
+      :id    => id,
+      :class => "tab-pane #{'active' if active}") do
+        yield
       end
     end
 
@@ -195,6 +317,14 @@ module BootstrapForm
           content_tag(:i, nil, class: "icon-time")
         end
       end
+    end
+
+    def fields_translated? object, fields
+      object.class.respond_to?(:translates?) && object.class.translates? && [fields].flatten.map{|field| object.translated_attribute_names.include?(field.to_sym)}.index(false).blank?
+    end
+
+    def any_errors_on? fields
+      fields.map{|name| object.errors[name].any?}.index(true).present?
     end
 
   end
